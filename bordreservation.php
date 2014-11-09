27b721fed4b3836700b233065e7b62a937057f76
Thraundil <emil.bak@hotmail.com>
2014-11-09 02:04:17 +0100

    hej

--- bordreservation.php
+++ bordreservation.php
@@ -6,7 +6,7 @@ include('variable.php');
 <div id="page">
     <div id="indhold">
         <div id="indholdText2">
-            <div id="indholdDiv2">
+            <div id="indholdDiv9">
                 <iframe scrolling="no" src="Tablereservation\index.php" style="width: 100%; height: 100%;"> </iframe>
             </div>
         </div>

4a6288f9e8f655860eb7dee2ddea0b0c6cf4e9b8
Thraundil <emil.bak@hotmail.com>
2014-11-09 17:21:00 +0100

    hej

--- top.php
+++ top.php
@@ -7,7 +7,7 @@ include("db.php");
 
 
 <head>
-    <title>DIKUlan PLAYGROUND</title>
+    <title>DIKU-LAN</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <link rel="stylesheet" type="text/css" href="MainCSS.css" />
 	

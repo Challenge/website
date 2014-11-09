500d6130d73293d2398978bcb853f81fb860d716
Thraundil <emil.bak@hotmail.com>
2014-11-09 17:16:13 +0100

    hej

--- vagtplan.php
+++ vagtplan.php
@@ -6,7 +6,7 @@ include('top.php');
             
 				<div id="indhold" style=" height: auto; overflow: hidden;>
 					<div id="indholdText2">
-						<div id="indholdDiv9">
+						<div id="indholdDiv9" style="text-align: center" ;>
 						
 						<iframe src="Projekter/Projekter.php" frameBorder="0" style="width: 800px; height: 600px;"> </iframe>
 						

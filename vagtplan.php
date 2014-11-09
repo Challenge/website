f05c324f30f5063f8fc159e08f12689cd1ab4e5f
Thraundil <emil.bak@hotmail.com>
2014-11-09 17:08:42 +0100

    hej

--- vagtplan.php
+++ vagtplan.php
@@ -8,25 +8,13 @@ include('top.php');
 					<div id="indholdText2">
 						<div id="indholdDiv2">
 						
-				<!-- Her skabes en table hvor informationen angående Vagtplan postes (fra databasen) -->
-						<table class="phpTable" >
-						<th> TID </th>
-						<th> Fredag </th>
-						<th> Lørdag </th>
-						<th> Søndag </th>
-
-					<?php	
-						$result = mysqli_query($db,"SELECT tid, fredag, lørdag, søndag FROM vagtplan");
-
-						while($row = mysqli_fetch_array($result))
-						{
-							echo "<tr> <td>" . $row['tid']    . "</td> 
-							      <td> <h3>" . $row['fredag'] . "</h3></td> 
-								  <td> <h3>" . $row['lørdag'] . "</h3></td> 								  
-								  <td> <h3>" . $row['søndag'] . "</h3></td> </tr> ";
-						}
-					?>
-						</table>	
+						<?php
+						
+						include('Projekter\Projekter.php');
+						
+						
+						?>
+						
 						</div>
 					</div>
 				</div>

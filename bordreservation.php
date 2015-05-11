3f6f2d8c9f73bd56a8b5ff524513e8be71889a77
Thraundil <emilsbak@gmail.com>
2015-05-11 02:11:02 +0200

    hej

--- bordreservation.php
+++ bordreservation.php
@@ -71,11 +71,7 @@ include('variable.php');
                                                            WHERE TicketID=$_POST[ticketID]");
 											
 										
-                                            mysqli_query($db, "UPDATE booking
-                                                           SET PlayerName='$_POST[playername]',
-                                                               TicketID='$_POST[ticketID]',
-                                                               Color='Red'
-                                                           WHERE SeatID=$id");
+
                                             echo "<script type='text/javascript'>alert('You have now booked seat " . $id . " ');</script>";
                                         } else {
                                             echo "<script type='text/javascript'>alert('You have to enter your name.');</script>";
